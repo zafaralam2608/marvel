10c25e1a9960c565cf676b2259ef80e7c4fd8e5c
Md Zafar Alam <zafaralam2608@gmail.com>
2022-06-13 00:11:08 +0530

    add repo name to asset links

--- src/main/constant/route.jsx
+++ src/main/constant/route.jsx
@@ -59,10 +59,10 @@ export const derived = [
 
 export const error = [
   {
-    link: '404', title: 'Page Not Found', message: 'The page you are trying to access is not available.', image: '%PUBLIC_URL%/assets/404.jpg',
+    link: '404', title: 'Page Not Found', message: 'The page you are trying to access is not available.', image: 'marvel/assets/404.jpg',
   },
   {
-    link: '500', title: 'Internal Server Error', message: 'Encountered an error while trying to access request', image: '%PUBLIC_URL%/assets/500.gif',
+    link: '500', title: 'Internal Server Error', message: 'Encountered an error while trying to access request', image: 'marvel/assets/500.gif',
   },
 ];
 
